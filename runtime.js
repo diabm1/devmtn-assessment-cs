2d6d315e91ff13acc7c073a54d2e5e754321eca0
diabm1 <diabm1855@gmail.com>
2023-05-21 16:43:14 -0400

    completed part 1

--- runtime.js
+++ runtime.js
@@ -1,38 +1,30 @@
-const perf = require('execution-time')();
+const perf = require("execution-time")();
 
+function doublerAppend(nums) {
+  let new_nums = [];
 
-function doublerAppend(nums){
-
-    let new_nums = [];
-
-    for (let i=0; i<nums.length; i++){
-        let num = nums[i] * 2;
-        new_nums.push(num);
-    }
-
+  for (let i = 0; i < nums.length; i++) {
+    let num = nums[i] * 2;
+    new_nums.push(num);
+  }
 }
 
+function doublerInsert(nums) {
+  let new_nums = [];
 
-function doublerInsert(nums){
-
-    let new_nums = [];
-
-    for (let i=0; i<nums.length; i++){
-        let num = nums[i] * 2;
-        new_nums.unshift(num);
-    }
-
+  for (let i = 0; i < nums.length; i++) {
+    let num = nums[i] * 2;
+    new_nums.unshift(num);
+  }
 }
 
-
-function getSizedArray(size){
-    let array = [];
-    for (let i=0; i<size; i++){
-        array.push(i);
-    }
-    return array
+function getSizedArray(size) {
+  let array = [];
+  for (let i = 0; i < size; i++) {
+    array.push(i);
+  }
+  return array;
 }
-
 
 const tinyArray = getSizedArray(10);
 const smallArray = getSizedArray(100);
@@ -40,23 +32,45 @@ const mediumArray = getSizedArray(1000);
 const largeArray = getSizedArray(10000);
 const extraLargeArray = getSizedArray(100000);
 
-
-
-// How long does it take to double every number in a given 
-// array? 
+// How long does it take to double every number in a given
+// array?
 
 // Try it with first function
-perf.start();                     // Starts timer
+perf.start(); // Starts timer
 doublerAppend(extraLargeArray);
-let resultsAppend = perf.stop();  // Stops timer and save time results
-
+let resultsAppend = perf.stop(); // Stops timer and save time results
 
 // Try it with second function
 perf.start();
 doublerInsert(extraLargeArray);
 let resultsInsert = perf.stop();
 
+// console.log("Results for the extraLargeArray");
+// console.log("insert", resultsInsert.preciseWords);
+// console.log("append", resultsAppend.preciseWords);
 
-console.log('Results for the extraLargeArray');
-console.log("insert", resultsInsert.preciseWords);
-console.log("append", resultsAppend.preciseWords);
+// array of the different sized arrays to test
+const arraysToTest = [
+  { name: "Tiny Array", array: tinyArray },
+  { name: "Small Array", array: smallArray },
+  { name: "Medium Array", array: mediumArray },
+  { name: "Large Array", array: largeArray },
+  { name: "Extra Array", array: extraLargeArray },
+];
+
+// loop over each array and measure the time for both functions
+for (let { name, array } of arraysToTest) {
+  console.log(`Results for ${name}`);
+
+  perf.start();
+  doublerAppend(array);
+  let resultsAppend = perf.stop();
+  console.log("append", resultsAppend.preciseWords);
+
+  perf.start();
+  doublerInsert(array);
+  let resultsInsert = perf.stop();
+  console.log("insert", resultsInsert.preciseWords);
+
+  console.log("\n"); // add an extra newline for readability
+}
